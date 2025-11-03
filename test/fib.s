1cb90be9b01035e94b2dfd3dae710ca97ae69072
Kunzo85 <165030288+Kunzo85@users.noreply.github.com>
2025-11-03 23:23:52 +0900

    test

--- test/fib.s
+++ test/fib.s
@@ -1,50 +1,47 @@
-.data
-.balign    8
-.text
-fib.10:                                  # !1
-    cmpl    $1, %eax                     # !2
-    jg    jle_else.24                    # !2
-    ret                                  # !2
-jle_else.24:                             # !2
-    movl    %eax, %ebx                   # !3
-    subl    $1, %ebx                     # !3
-    movl    %eax, 0(%ebp)                # !3
-    movl    %ebx, %eax                   # !3
-    addl    $8, %ebp                     # !3
-    call    fib.10                       # !3
-    subl    $8, %ebp                     # !3
-    movl    0(%ebp), %ebx                # !3
-    subl    $2, %ebx                     # !3
-    movl    %eax, 4(%ebp)                # !3
-    movl    %ebx, %eax                   # !3
-    addl    $8, %ebp                     # !3
-    call    fib.10                       # !3
-    subl    $8, %ebp                     # !3
-    movl    4(%ebp), %ebx                # !3
-    addl    %ebx, %eax                   # !3
-    ret                                  # !3
-.globl    min_caml_start
+.section	".rodata"
+.align	8
+.section	".text"
+fib.10:			!1
+	cmp	%i2, 1			!2
+	bg	ble_else.24
+	nop
+	retl
+	nop
+ble_else.24:
+	sub	%i2, 1, %i3			!3
+	st	%i2, [%i0 + 0]			!3
+	mov	%i3, %i2			!3
+	st	%o7, [%i0 + 4]			!3
+	call	fib.10			!3
+	add	%i0, 8, %i0	! delay slot			!3
+	sub	%i0, 8, %i0			!3
+	ld	[%i0 + 4], %o7			!3
+	ld	[%i0 + 0], %i3			!3
+	sub	%i3, 2, %i3			!3
+	st	%i2, [%i0 + 4]			!3
+	mov	%i3, %i2			!3
+	st	%o7, [%i0 + 12]			!3
+	call	fib.10			!3
+	add	%i0, 16, %i0	! delay slot			!3
+	sub	%i0, 16, %i0			!3
+	ld	[%i0 + 12], %o7			!3
+	ld	[%i0 + 4], %i3			!3
+	add	%i3, %i2, %i2			!3
+	retl
+	nop
+.global	min_caml_start
 min_caml_start:
-.globl    _min_caml_start
-_min_caml_start: # for cygwin
-    pushl    %eax
-    pushl    %ebx
-    pushl    %ecx
-    pushl    %edx
-    pushl    %esi
-    pushl    %edi
-    pushl    %ebp
-    movl    32(%esp),%ebp
-    movl    36(%esp),%eax
-    movl    %eax,min_caml_hp
-    movl    $30, %eax                    # !4
-    call    fib.10                       # !4
-    call    min_caml_print_int           # !4
-    popl    %ebp
-    popl    %edi
-    popl    %esi
-    popl    %edx
-    popl    %ecx
-    popl    %ebx
-    popl    %eax
-    ret
+	save	%sp, -112, %sp
+	set	30, %i2			!4			!4
+	st	%o7, [%i0 + 4]			!4
+	call	fib.10			!4
+	add	%i0, 8, %i0	! delay slot			!4
+	sub	%i0, 8, %i0			!4
+	ld	[%i0 + 4], %o7			!4
+	st	%o7, [%i0 + 4]			!4
+	call	min_caml_print_int			!4
+	add	%i0, 8, %i0	! delay slot			!4
+	sub	%i0, 8, %i0			!4
+	ld	[%i0 + 4], %o7			!4
+	ret
+	restore
